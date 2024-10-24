ed727c8321584cf994d58bf0aca0e87cf00606e7
Htaw Mon <htawmon123@gmail.com>
2024-10-23 23:38:48 -0400

    Add codes to loading.tsx

---
+++ app/components/weather/loading.tsx
@@ -0,0 +1,7 @@
+// app/components/weather/loading.tsx
+
+// This component will be shown while the weather data is being fetched
+export default function WeatherLoading() {
+    return <h1>Loading...</h1>; // Simple loading message
+  }
+  
