65594d75d529b67a49c2291638dec6d347a403c7
iammon <htawmon123@gmail.com>
2024-10-31 01:18:57 -0400

    Add recursive GCD prototype and user input validation prototype

---
+++ recursion/include/gcd.hpp
@@ -0,0 +1,10 @@
+#ifndef GCD_HPP
+#define GCD_HPP
+
+// Function to find the Greatest Common Divisor (GCD) of two integers
+int gcd(int a, int b);
+
+// Function to get a valid integer input from the user
+int getValidInteger();
+
+#endif // GCD_HPP
